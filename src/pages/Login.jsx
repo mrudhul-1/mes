153f2cfa423f0b1a4e4e457dfef554905e8a21b0
mrudhul-1 <bmrudhul6@gmail.com>
2026-03-18 19:21:09 +0530

    done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ export default function Login() {
       return
     }
 
-    if (!email.includes("@")){
+    if (!email.includes("@gmail.com")){
       toast.error("Invalid email formate")
     }
     // console.log({ email, password });
